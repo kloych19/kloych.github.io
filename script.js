f18557b00e011c44c749f25af13987140cf50df9
kloych19 <kjl100@juniata.edu>
2026-05-04 09:52:19 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function showTemp(bubbleId) {
-  const bubbles = document.querySelectorAll("#tempBubble"); 
-  bubbles.forEach(b => b.style.display = "none");
-  document.getElementById(bubbleId).style.display = "block";
-}
-
-
-
